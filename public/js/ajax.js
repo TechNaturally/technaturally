78738d91d1a4bbff0d2fd3aa65b10b6d7167a4fa
Pat Long <plong00@gmail.com>
2018-01-18 23:53:43 -0700

    Helpers: ajax function and Element.closest polyfill

---
+++ public/js/ajax.js
@@ -0,0 +1,63 @@
+/* Technaturally.com/js/ajax.js
+
+Simple, cross-browser Javascript POST/GET xhr request object. Supports request data and proper AJAX headers.
+
+*/
+
+/*
+ * source: https://stackoverflow.com/a/38931547/1798697
+ *
+ */
+function urlencodeFormData(fd){
+    var s = '';
+    function encode(s){ return encodeURIComponent(s).replace(/%20/g,'+'); }
+    for(var pair of fd.entries()){
+        if(typeof pair[1]=='string'){
+            s += (s?'&':'') + encode(pair[0])+'='+encode(pair[1]);
+        }
+    }
+    return s;
+}
+
+/** 
+ * Adapted from source: https://gist.github.com/Xeoncross/7663273
+ *
+ * IE 5.5+, Firefox, Opera, Chrome, Safari XHR object
+ * 
+ * @param string url
+ * @param object callback
+ * @param mixed data
+ * @param mixed config
+ * @param null x
+ */
+function ajax(url, callback, data, config, x) {
+  try {
+    config = config || {};
+    x = new(this.XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0');
+    x.open(config.method ? config.method : (data ? 'POST' : 'GET'), url, 1);
+    x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    x.setRequestHeader('Content-type', config.multipart ? 'multipart/form-data' : 'application/x-www-form-urlencoded');
+    x.onreadystatechange = function () {
+      x.readyState > 3 && callback && callback(x.responseText, x);
+    };
+    if (data.constructor.name == 'FormData' && !config.multipart) {
+      data = urlencodeFormData(data);
+    }
+    x.send(data);
+  } catch (e) {
+    window.console && console.log(e);
+  }
+};
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+// https://gist.github.com/jed/993585
+// https://gist.github.com/Fluidbyte/5082377
+// https://github.com/Xeoncross/kb_javascript_framework/blob/master/kB.js#L30
+// https://gist.github.com/iwek/5599777
+// http://msdn.microsoft.com/en-us/library/ms537505(v=vs.85).aspx#_id
+
+// @todo look into lengthComputable for xhr.upload browsers
+// http://stackoverflow.com/questions/11127654/why-is-progressevent-lengthcomputable-false
+// http://stackoverflow.com/questions/10956574/why-might-xmlhttprequest-progressevent-lengthcomputable-be-false
+// https://github.com/ForbesLindesay/ajax/blob/master/index.js
